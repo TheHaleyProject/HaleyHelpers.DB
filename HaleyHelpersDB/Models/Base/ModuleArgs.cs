272c22803239448e344309f4c11bf63cbc96d90f
LingamHPZ4 <rmsmech@gmail.com>
2025-06-26 11:35:23 +0400

    version changes

--- HaleyHelpersDB/Models/Base/ModuleArgs.cs
+++ HaleyHelpersDB/Models/Base/ModuleArgs.cs
@@ -2,6 +2,7 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection.Metadata.Ecma335;
 using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
@@ -13,10 +14,22 @@ namespace Haley.Models
         public object[] Arguments { get; protected set; }
         public void ClearParameters() => ClearParametersInternal(true);
         public void ClearParameters(string groupKey) => ClearParametersInternal(groupKey);
-        public void UpsertParameter(string key, object value, bool replace = true) => AddParameterInternal(key, value, replace);
-        public void UpsertParameter(string groupKey, string key, object value, bool replace = true) => AddParameterInternal(groupKey, key, value, replace);
-        public void SetParameters(Dictionary<string, object> parameters) => SetParametersInternal(parameters);
-        public void SetParameters(string groupKey, Dictionary<string, object> parameters) => SetParametersInternal(groupKey, parameters);
+        public IModuleArgs UpsertParameter(string key, object value, bool replace = true) {
+            AddParameterInternal(key, value, replace);
+            return this; 
+        }
+        public IModuleArgs UpsertParameter(string groupKey, string key, object value, bool replace = true) {
+            AddParameterInternal(groupKey, key, value, replace);
+            return this;
+        }
+        public IModuleArgs SetParameters(Dictionary<string, object> parameters) {
+            SetParametersInternal(parameters);
+            return this;
+        }
+        public IModuleArgs SetParameters(string groupKey, Dictionary<string, object> parameters) {
+            SetParametersInternal(groupKey, parameters);
+            return this;
+        }
         public bool TransactionMode { get; set; }
         public static void Assert(params object[] input) {
             if (input.Any(p => p == null)) throw new ArgumentNullException("Required input object is missing");
