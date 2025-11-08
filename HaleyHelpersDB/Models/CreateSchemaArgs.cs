12565cb509e16abb39b87aab46a13aafcd2cc10b
Quetzal <rmsmech@outlook.com>
2025-11-08 14:46:38 +0400

    Create Schema in HaleyDB

--- HaleyHelpersDB/Models/CreateSchemaArgs.cs
+++ HaleyHelpersDB/Models/CreateSchemaArgs.cs
@@ -4,14 +4,15 @@ using System.Globalization;
 
 namespace Haley.Models
 {
-	public class LoadSqlArgs {
+	public class CreateSchemaArgs {
         public string Key { get; set; }
         public string FallBackDBName { get; set; }
         internal string DBName { get; set; }
         public string SQLPath { get; set; }
+        public string SQLContent { get; set; }
         public Dictionary<string, string> VariablesToReplace { get; set; } = new Dictionary<string, string>();
         public TargetDB TargetDB { get; set; } = TargetDB.maria;
-        public LoadSqlArgs(string adapter_key) { Key = adapter_key;
+        public CreateSchemaArgs(string adapter_key) { Key = adapter_key;
         }
 	}
 }
--- HaleyHelpersDB/Utils/Utils.cs
+++ HaleyHelpersDB/Utils/Utils.cs
@@ -6,6 +6,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Net;
 using System.Reflection;
+using System.Reflection.Metadata;
+using System.Runtime.InteropServices;
 using System.Text;
 using System.Text.RegularExpressions;
 using System.Xml.Linq;
@@ -15,7 +17,7 @@ namespace Haley.Utils
 {
     public static class CreationUtils
     {
-        public static async Task<IFeedback> LoadSQL(this IAdapterGateway agw,LoadSqlArgs args) {
+        public static async Task<IFeedback> CreateSchema(this IAdapterGateway agw,CreateSchemaArgs args) {
             var fb = new Feedback().SetSource("HALEY-DB-UTILS");
             try {
                 if (args == null || string.IsNullOrWhiteSpace(args.Key)) throw new ArgumentNullException("Key cannot be empty.");
@@ -27,7 +29,7 @@ namespace Haley.Utils
                 switch (args.TargetDB) {
                     case Enums.TargetDB.maria:
                     case Enums.TargetDB.mysql:
-                    return await agw.LoadSQLMaria(args);
+                    return await agw.CreateMariaSchema(args);
                     default:
                     throw new NotImplementedException($@"The target db type {args.TargetDB} is not implemented for loading the initial sql.");
                 }
@@ -35,31 +37,38 @@ namespace Haley.Utils
                 return fb.SetMessage(ex.Message).SetTrace(ex.StackTrace).SetCode((int)HttpStatusCode.InternalServerError);
             }
         }
-        static async Task<IFeedback> LoadSQLMaria(this IAdapterGateway agw, LoadSqlArgs args) {
+        static async Task<IFeedback> CreateMariaSchema(this IAdapterGateway agw, CreateSchemaArgs args) {
             var fb = new Feedback().SetSource("HALEY-DB-MARIA");
             if (!File.Exists(args.SQLPath)) throw new ArgumentException($@"SQL file is not found in {args.SQLPath}. Please check..");
-            var exists = await agw.Scalar(new AdapterArgs(args.Key) { ExcludeDBInConString = true, Query = QRY_MARIA.SCHEMA_EXISTS }, (NAME, args.DBName));
-            if (exists != null && exists.IsNumericType()) return fb.SetStatus(true).SetMessage($@"Database {args.DBName} already exists. No action taken.").SetCode((int)HttpStatusCode.Continue);
-            
-            //if the file exists, then run this file against the adapter gateway but ignore the db name.
-            var content = File.ReadAllText(args.SQLPath);
-            //We know that the file itself contains "dss_core" as the schema name. Replace that with new one.
 
+            string content = args.SQLContent;
+
+            if (string.IsNullOrWhiteSpace(content) && string.IsNullOrWhiteSpace(args.SQLPath)) throw new ArgumentNullException("Either SQL content or SQL path must be provided.");
+
+            if (string.IsNullOrWhiteSpace(content)) {
+                //if the file exists, then run this file against the adapter gateway but ignore the db name.
+                content = File.ReadAllText(args.SQLPath);
+            }
+           
             if (args.VariablesToReplace != null && args.VariablesToReplace.Count > 0) {
                 foreach (var kvp in args.VariablesToReplace) {
                     if (string.IsNullOrWhiteSpace(kvp.Key)) continue;
                     content = content.Replace(kvp.Key, kvp.Value);
                 }
             }
-            
+
+            var exists = await agw.Scalar(new AdapterArgs(args.Key) { ExcludeDBInConString = true, Query = QRY_MARIA.SCHEMA_EXISTS }, (NAME, args.DBName));
+            if (exists != null && exists.IsNumericType()) return fb.SetStatus(true).SetMessage($@"Database {args.DBName} already exists. No action taken.").SetCode((int)HttpStatusCode.Continue);
+
             //?? Should we run everything in one go or run as separate statements???
             //if the input contains any delimiter or procedure, remove them.
+
             object queryContent = content;
             List<string> procedures = new();
             if (content.Contains("Delimiter", StringComparison.InvariantCultureIgnoreCase)) {
                 //Step 1 : Remove delimiter lines
                 content = Regex.Replace(content, @"DELIMITER\s+\S+", "", RegexOptions.IgnoreCase); //Remove the delimiter comments
-                //Step 2 : Remove version-specific comments
+                                                                                                   //Step 2 : Remove version-specific comments
                 content = Regex.Replace(content, @"/\*!.*?\*/;", "", RegexOptions.Singleline);
                 //Step 3 : Extract all Procedures
                 string pattern = @"CREATE\s+PROCEDURE.*?END\s*//";
@@ -83,6 +92,7 @@ namespace Haley.Utils
                     await agw.NonQuery(new AdapterArgs(args.Key) { ExcludeDBInConString = true, Query = procedures.ToArray() }.ForTransaction(handler));
                 }
             }
+
             return fb.SetStatus(true).SetCode((int)HttpStatusCode.Created);
         }
     }
